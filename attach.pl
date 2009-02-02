b5f02486b113f9a58a8a3d37e1bf8ea97564ba0d
satzz21@gmail.com <satodaakihiko@EM114-51-129-148.pool.e-mobile.ne.jp>
2009-02-02 18:35:29 +0900

    given data for cell & sample

---
+++ attach.pl
@@ -0,0 +1,10 @@
+use strict;
+use warnings;
+use lib qw{./lib};
+use FindBin qw/$Bin/;
+use IO::File;
+use Data::Dumper;
+use Path::Class;
+use Q10::MoCo;
+
+warn Dumper moco('Sample')->retrieve_all;
