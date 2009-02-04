6c0a342c361c8554c5b47e61ef58d2d777e86949
satzz21@gmail.com <satodaakihiko@EM114-51-5-57.pool.e-mobile.ne.jp>
2009-02-04 17:33:14 +0900

    add Q10::MoCo::TempertureP8Ratio

---
+++ lib/Q10/MoCo/TempertureP8Ratio.pm
@@ -0,0 +1,13 @@
+package Q10::MoCo::TempertureP8Ratio;
+use strict;
+use warnings;
+use FindBin qw/$Bin/;
+use IO::File;
+use Data::Dumper;
+use Path::Class;
+use Q10::MoCo;
+use base moco;
+
+__PACKAGE__->table('temperture_p8_ratio');
+
+1;
