14542f2678a5f8c73f5d1c5982164ae7247f4d86
satzz21@gmail.com <satodaakihiko@EM114-51-5-57.pool.e-mobile.ne.jp>
2009-02-04 17:22:54 +0900

    modify attach_asc.pl

--- attach_asc.pl
+++ attach_asc.pl
@@ -107,8 +107,8 @@ set xrange [0.01:1]
 fit y_0 + A * exp(-(x/tau)** beta) '$correlation_dat_file_name' via y_0, A, tau, beta
 },
             param_file_name => $param_file_name,
-            param           => 'tau',
-        );
+            param           => [qw/tau/],
+        )->{tau};
         $dls_trial->relaxation_time($tau);
         $correlation_plt_file->print (<<EOD);
 set term postscript
