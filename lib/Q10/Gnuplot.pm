ef98daded9d6659c153fd1891e71fc6479b87b23
satzz21@gmail.com <satodaakihiko@apple-of-satzz.local>
2009-02-08 22:59:40 +0900

    modify

--- lib/Q10/Gnuplot.pm
+++ lib/Q10/Gnuplot.pm
@@ -24,27 +24,20 @@ sub get_param {
     my $class = shift;
     my %arg = @_;
     my $statement       = $arg{statement} or return;
-    my $param           = $arg{param} or return;
     my $fit_file_name   = $arg{fit_file_name} || dir($Bin, 'temp')->file('fit.plt');
     my $log_file_name   = $arg{log_file_name} || dir($Bin, 'temp')->file('log.txt');
     my $param_file_name = $arg{param_file_name} || dir($Bin, 'temp')->file('param.txt');
     my $fit_file        = IO::File->new($fit_file_name, 'w');
-    $fit_file->print (qq{
-$statement
+    $fit_file->print (qq{$statement
 save var '$param_file_name'
 });
     $fit_file->close;
     system qq{gnuplot $fit_file_name &>$log_file_name};
-    my $param_file = IO::File->new($param_file_name);
-    if ($param && ref $param eq 'ARRAY') {
-        my $res = {};
-        for my $each_param (@$param) {
-            my ($line) = grep {/$each_param/} <$param_file>;
-            my ($val)  = $line =~ /$each_param \s+ = \s+ (.+) /x;
-            $res->{$each_param} = $val;
-        }
-        return $res;
-    }
+    my $res = {};
+    my $param;
+    my $content = $param_file_name->slurp;
+    my %param = $content =~ /(\w+) \s+ = \s+ (.+) /xg;
+    return \%param;
 }
 
 1;
