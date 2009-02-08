3b505a6cd01b5e9b3647626b7769a521d84247ce
satzz21@gmail.com <satodaakihiko@apple-of-satzz.local>
2009-02-08 23:00:45 +0900

    modify

--- relaxation_time_fitting.pl
+++ relaxation_time_fitting.pl
@@ -13,37 +13,38 @@ my $plt_dir = dir($Bin, qw/ graph plt /);
 my $param_dir = dir($Bin, qw/ graph param /);
 my $log_dir = dir($Bin, qw/ graph log /);
 
-moco('TempertureP8Ratio')->retrieve_all()->each(sub {$_->delete});
+moco('TempertureP8Ratio')->retrieve_all->each(sub {$_->delete});
 my @temperture = map {$_->temperture} moco('DLSTrial')->search(field => 'distinct temperture');
 for my $temperture (@temperture) {
     warn $temperture;
     my $dat_file_name = $dat_dir->file(sprintf 'temperture_%s.dat', $temperture);
+    my $dat_ln_file_name = $dat_dir->file(sprintf 'temperture_%s_ln.dat', $temperture);
     my $dat_file = IO::File->new($dat_file_name, 'r');
     my @line = grep {/\% P8/} <$dat_file>;
     my @p8_ratio;
     for my $line (@line) {
-        my ($p8_ratio) = $line =~ /(.+)\%/;
+        my ($p8_ratio) = $line =~ /([\d.]+)\%/;
         push @p8_ratio, $p8_ratio;
     }
     my $ind = 0;
     for my $p8_ratio (@p8_ratio) {
+        warn $p8_ratio;
         my $log_file_name = $log_dir->file(sprintf 'temperture_p8_ratio_%s_%s.txt', $temperture, $p8_ratio);
         my $param_file_name = $param_dir->file(sprintf 'temperture_p8_ratio_%s_%s.txt', $temperture, $p8_ratio);
         my $fit_file_name = $plt_dir->file(sprintf 'temperture_p8_ratio_%s_%s.plt', $temperture, $p8_ratio);
         my $res = Q10::Gnuplot->get_param(
             fit_file_name   => $fit_file_name,
             log_file_name   => $log_file_name,
-            statement       => qq{
-fit a * x ^ b '$dat_file_name' ind $ind via a, b
-},
+            statement       => qq{fit a + x * b '$dat_ln_file_name' ind $ind via a, b \n},
             param_file_name => $param_file_name,
-            param           => [qw/a b/],
         );
+        my $a = $res->{a} or next;
+        my $b = $res->{b} or next;
         my $record = moco('TempertureP8Ratio')->create(
             temperture => $temperture,
             p8_ratio   => $p8_ratio,
-            a          => $res->{a},
-            b          => $res->{b}
+            a          => $a,
+            b          => $b,
         );
         $ind++;
     }
