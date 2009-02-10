2519184e5ecba3320352a0a1fd9a037c3b699d6f
satzz21@gmail.com <satodaakihiko@EM114-51-154-87.pool.e-mobile.ne.jp>
2009-02-10 17:10:05 +0900

    modify

--- lib/Q10/Divider.pm
+++ lib/Q10/Divider.pm
@@ -6,16 +6,14 @@ use IO::File;
 use Data::Dumper;
 use Path::Class;
 
-use UNIVERSAL::require;
-use Exporter::Lite;
-
-our @EXPORT = qw/divider/;
-
-sub divider (@) {
-    my $divider = shift or return __PACKAGE__;
-    $divider = "Q10::Divider::$divider";
-    $divider->require or die $@;
-    $divider;
+sub new {
 }
+
+sub get_html {
+}
+
+sub run {
+}
+
 
 1;
