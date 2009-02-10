087edd7c1290c27ebae09a85cc63e7611c08ae91
satzz21@gmail.com <satodaakihiko@EM114-51-154-87.pool.e-mobile.ne.jp>
2009-02-10 17:25:36 +0900

    define interface of Q10::Divider

--- temperture_and_rotation_angle.pl
+++ temperture_and_rotation_angle.pl
@@ -14,8 +14,28 @@ my $html_file_name = html_dir->file('index.html');
 my $html_file = IO::File->new($html_file_name, 'w');
 my $html = '';
 
-$html .= divider('Temperture')->get_html;
-$html .= divider('RotationAngle')->get_html;
+my $divider1 = Q10::Divider->new(
+    divider => 'temperture',
+    key     => 'p8_ratio',
+    x       => 'k',
+    y       => 'relaxation_time inverse'
+);
+$divider1->logscale(qw/x y/);
+$divider1->xrange(0.004, 0.01);
+$divider1->run;
+$html .= $divider1->get_html;
+
+my $divider2 = Q10::Divider->new(
+    divider => 'rotation_angle',
+    key     => 'p8_ratio',
+    x       => 'temperture',
+    y       => 'relaxation_time inverse'
+);
+$divider2->logscale(qw/x y/);
+$divider2->xrange(60, 160);
+$divider2->yrange(0, 100);
+$divider2->run;
+$html .= $divider2->get_html;
 
 $html_file->print($html);
 $html_file->close;
