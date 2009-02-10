f946c9d491bdfa965466ace2b107890975b12e2e
satzz21@gmail.com <satodaakihiko@EM114-51-154-87.pool.e-mobile.ne.jp>
2009-02-10 17:34:16 +0900

    modify interface

--- temperture_and_rotation_angle.pl
+++ temperture_and_rotation_angle.pl
@@ -15,25 +15,25 @@ my $html_file = IO::File->new($html_file_name, 'w');
 my $html = '';
 
 my $divider1 = Q10::Divider->new(
-    divider => 'temperture',
-    key     => 'p8_ratio',
-    x       => 'k',
-    y       => 'relaxation_time inverse'
+    divider  => 'temperture',
+    key      => 'p8_ratio',
+    x        => 'k',
+    y        => 'relaxation_time inverse'
+    logscale => [qw/x y/],
+    xrange   => [0.004, 0.01];
 );
-$divider1->logscale(qw/x y/);
-$divider1->xrange(0.004, 0.01);
 $divider1->run;
 $html .= $divider1->get_html;
 
 my $divider2 = Q10::Divider->new(
-    divider => 'rotation_angle',
-    key     => 'p8_ratio',
-    x       => 'temperture',
-    y       => 'relaxation_time inverse'
+    divider  => 'rotation_angle',
+    key      => 'p8_ratio',
+    x        => 'temperture',
+    y        => 'relaxation_time inverse'
+    logscale => [qw/y/],
+    xrange   => [60, 160];
+    yrange   => [0, 100];
 );
-$divider2->logscale(qw/x y/);
-$divider2->xrange(60, 160);
-$divider2->yrange(0, 100);
 $divider2->run;
 $html .= $divider2->get_html;
 
