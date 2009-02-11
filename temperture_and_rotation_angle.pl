181bf6b76af18dc2b8ace76adaaf7f195046dc85
satzz21@gmail.com <satodaakihiko@apple-of-satzz.local>
2009-02-11 10:38:31 +0900

    modify interface

--- temperture_and_rotation_angle.pl
+++ temperture_and_rotation_angle.pl
@@ -14,25 +14,23 @@ my $html_file_name = html_dir->file('index.html');
 my $html_file = IO::File->new($html_file_name, 'w');
 my $html = '';
 
-my $divider1 = Q10::Divider->new(
-    divider  => 'temperture',
-    key      => 'p8_ratio',
-    x        => 'k',
-    y        => 'relaxation_time inverse',
-    logscale => [qw/x y/],
-    range    => {x => [0.004, 0.01]},
-);
+my $divider1 = Q10::Divider->new;
+$divider1->divider = 'temperture';
+$divider1->key      = 'p8_ratio';
+$divider1->x        = 'rotation_angle';
+$divider1->y        = 'relaxation_time inverse';
+$divider1->logscale = {x => 1, y => 1};
+$divider1->range    = {x => [0.004, 0.01]};
 $divider1->run;
 $html .= $divider1->get_html;
 
-my $divider2 = Q10::Divider->new(
-    divider  => 'rotation_angle',
-    key      => 'p8_ratio',
-    x        => 'temperture',
-    y        => 'relaxation_time inverse'
-    logscale => [qw/y/],
-    range    => {x => [60, 160], y => [0, 100]},
-);
+my $divider2 = Q10::Divider->new;
+$divider2->divider = 'rotation_angle';
+$divider2->key      = 'p8_ratio';
+$divider2->x        = 'temperture';
+$divider2->y        = 'relaxation_time inverse';
+$divider2->logscale = {y => 1};
+$divider2->range    = {x => [60, 160], y => [0.01, 100]};
 $divider2->run;
 $html .= $divider2->get_html;
 
