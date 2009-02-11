18fe244d91df5d6557ad9edb14d0e71b3cf4c785
satzz21@gmail.com <satodaakihiko@apple-of-satzz.local>
2009-02-11 10:57:11 +0900

    modify interface

--- relaxation_time_ensemble.pl
+++ relaxation_time_ensemble.pl
@@ -10,51 +10,12 @@ use Q10::Config;
 use Q10::Divider;
 use Q10::Gnuplot;
 
-{
-    my $index = -1;
-    my @plot;
-    my $plot;
-    my $out;
-    my ($old_p8_ratio, $p8_ratio);
-    my @dls_trial = moco('DLSTrialCellSample')->search(order => 'p8_ratio desc');
-    my $dat_file_name = $dat_dir->file('relaxation_time_ensemble_p8_ratio.dat');
-    my $plt_file_name = $plt_dir->file('relaxation_time_ensemble_p8_ratio.plt');
-    my $ps_file_name  = $ps_dir->file('relaxation_time_ensemble_p8_ratio.ps');
-    my $img_file_name  = $img_dir->file('relaxation_time_ensemble_p8_ratio.png');
-    my $dat_file = IO::File->new($dat_file_name, 'w');
-    my $plt_file = IO::File->new($plt_file_name, 'w');
-    for my $dls_trial (@dls_trial) {
-        $dls_trial->date eq '2009-01-29' and next;
-        my $p8_ratio = $dls_trial->p8_ratio;
-        if (!defined $old_p8_ratio or $old_p8_ratio != $p8_ratio) {
-            $out .= "\n\n" if defined $old_p8_ratio;
-            $out .= sprintf "# %s%% P8\n", $p8_ratio;
-            $index++;
-            push @plot, sprintf qq{'%s' ind %s t '%s%% P8'}, $dat_file_name, $index, $p8_ratio;
-        }
-        my $relaxation_time = $dls_trial->relaxation_time or next;
-        my $count_rate_max = $dls_trial->count_rate_max or next;
-        $out .= sprintf "%s\t%s\n", $count_rate_max, $relaxation_time;
-        $old_p8_ratio = $p8_ratio;
-    }
-    $plot = join ',', @plot;
-    $dat_file->print($out);
-    $dat_file->close;
-    $dat_file->close;
-    $out = qq{
-set term postscript
-set key outside
-set size 1.5,1
-set xlabel 'Count Rate Max'
-set ylabel 'Relaxation Time[ms]'
-set xrange[3:2000]
-set yrange[0.01:2]
-set logscale x
-set logscale y
-set output '$ps_file_name'
-plot $plot
-};
-    $plt_file->print($out);
-    Q10::Gnuplot->run($plt_file_name);
-    Q10::Gnuplot->convert($ps_file_name, $img_file_name);
-}
+my $divider1 = Q10::Divider->new;
+$divider1->divider = 'sample_angle';
+$divider1->key      = 'p8_ratio';
+$divider1->x        = 'count_rate_max';
+$divider1->y        = 'relaxation_time inverse';
+$divider1->logscale = {x => 1, y => 1};
+$divider1->size     = {x => 1.5};
+$divider1->range    = {x => [3, 2000], y => [0.03, 100]};
+$divider1->run;
