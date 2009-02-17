e64e4c66dfef1bb74f54672163477eb3f3b7979c
satzz21@gmail.com <satodaakihiko@EM114-51-156-73.pool.e-mobile.ne.jp>
2009-02-17 21:27:41 +0900

    modify count_rate_max.pl

--- count_rate_max.pl
+++ count_rate_max.pl
@@ -10,6 +10,10 @@ use Q10::Config;
 use Q10::Divider;
 use Q10::Gnuplot;
 
+my $html_file_name = html_dir->file('count_rate_max.html');
+my $html_file = IO::File->new($html_file_name, 'w');
+my $html = '';
+
 my $divider1 = Q10::Divider->new;
 $divider1->divider = 'sample_angle';
 $divider1->key      = 'p8_ratio';
@@ -17,5 +21,8 @@ $divider1->x        = 'count_rate_max';
 $divider1->y        = 'relaxation_time inverse';
 $divider1->logscale = {x => 1, y => 1};
 $divider1->size     = {x => 1.5};
-$divider1->range    = {x => [3, 2000], y => [0.03, 100]};
+$divider1->range    = {x => [3, 2000], y => [1, 100]};
 $divider1->run;
+
+$html_file->print($divider1->get_html);
+
