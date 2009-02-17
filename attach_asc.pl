4e402f57fe89573fd5071cd3bf99ace761d4a40b
satzz21@gmail.com <satodaakihiko@EM114-51-156-73.pool.e-mobile.ne.jp>
2009-02-17 21:27:12 +0900

    modify attach_asc.pl

--- attach_asc.pl
+++ attach_asc.pl
@@ -6,26 +6,21 @@ use IO::File;
 use Data::Dumper;
 use Path::Class;
 use Q10::MoCo;
+use Q10::Config;
 use Q10::Gnuplot;
 
-my $dat_dir = dir($Bin, qw/ graph dat /);
-my $plt_dir = dir($Bin, qw/ graph plt /);
-my $ps_dir  = dir($Bin, qw/ graph ps /);
-my $img_dir = dir($Bin, qw/ graph img /);
-my $param_dir = dir($Bin, qw/ graph param /);
-my $log_dir = dir($Bin, qw/ graph log /);
-moco('DLSTrial')->retrieve_all->each(sub {$_->delete});
-unlink $dat_dir->file($_) for grep {/dat/} $dat_dir->open->read;
-unlink $plt_dir->file($_) for grep {/plt/} $plt_dir->open->read;
-unlink $ps_dir->file($_) for grep {/ps/} $ps_dir->open->read;
-unlink $param_dir->file($_) for grep {/txt/} $param_dir->open->read;
-unlink $log_dir->file($_) for grep {/txt/} $log_dir->open->read;
-my $asc_dir = dir($Bin, qw/ DLS  asc /);
-my $graph_html;
-for my $date (sort grep {/\d+/} $asc_dir->open->read) {
+# moco('DLSTrial')->retrieve_all->each(sub {$_->delete});
+# unlink dat_dir->file($_) for grep {/dat/} dat_dir->open->read;
+# unlink plt_dir->file($_) for grep {/plt/} plt_dir->open->read;
+# unlink ps_dir->file($_) for grep {/ps/} ps_dir->open->read;
+# unlink param_dir->file($_) for grep {/txt/} param_dir->open->read;
+# unlink log_dir->file($_) for grep {/txt/} log_dir->open->read;
+# my $graph_html;
+for my $date (sort grep {/\d+/} asc_dir->open->read) {
+    $date eq '090215' or $date eq '090217' or next;
     my ($year, $month, $day) = $date =~ /^(\d{2})(\d{2})(\d{2})$/;
     $day or next;
-    my $date_dir = $asc_dir->subdir($date);
+    my $date_dir = asc_dir->subdir($date);
     for my $asc_file_name (sort grep {/ASC/} $date_dir->open->read) {
         warn sprintf '%s', $date_dir->file($asc_file_name);
         my $asc = $date_dir->file($asc_file_name);
@@ -48,7 +43,7 @@ for my $date (sort grep {/\d+/} $asc_dir->open->read) {
                 $flag = 3;
                 next;
             }
-            my ($time, $val) = $line =~ /([.\dE\-]+)\s+([.\dE\-]+)/;
+            my ($time, $val) = $line =~ /([.\dE\-\+]+)\s+([.\dE\-\+]+)/;
             if ($flag == 1) {
                 if (defined $time) {
                     push @correlation, sprintf "%s\t%s\n", $time, $val;
@@ -63,9 +58,23 @@ for my $date (sort grep {/\d+/} $asc_dir->open->read) {
                 $count_rate_min = $val if $count_rate_min > $val;
             }
         }
-        my $dls_trial = moco('DLSTrial')->create(
+        my $date_val = (sprintf '20%s-%s-%s', $year, $month, $day);
+        my $dls_trial = moco('DLSTrial')->search(
+            where => {
+                cell_id             => $cell_id,
+                date                => $date_val,
+                temperture          => $temperture,
+                rotation_angle      => $rotation_angle,
+                sample_angle        => $sample_angle,
+                laser_position      => $laser_position,
+                nd_filter_position  => $nd_filter_position,
+                polarizer_angle     => $polarizer_angle,
+                sample_position     => $sample_position,
+            }
+        )->first;
+        $dls_trial or $dls_trial = moco('DLSTrial')->create(
             cell_id             => $cell_id,
-            date                => (sprintf '20%s-%s-%s', $year, $month, $day),
+            date                => $date_val,
             temperture          => $temperture,
             rotation_angle      => $rotation_angle,
             sample_angle        => $sample_angle,
@@ -78,23 +87,25 @@ for my $date (sort grep {/\d+/} $asc_dir->open->read) {
             correlation_max     => $correlation_max,
         );
         my $dls_trial_id = $dls_trial->dls_trial_id;
-        my $correlation_dat_file_name = $dat_dir->file(sprintf 'correlation_dls_%s.dat', $dls_trial_id);
-        my $count_rate_dat_file_name  = $dat_dir->file(sprintf 'count_rate_dls_%s.dat', $dls_trial_id);
-        my $correlation_plt_file_name = $plt_dir->file(sprintf 'correlation_dls_%s.plt', $dls_trial_id);
-        my $count_rate_plt_file_name  = $plt_dir->file(sprintf 'count_rate_dls_%s.plt', $dls_trial_id);
-        my $correlation_ps_file_name  = $ps_dir->file(sprintf 'correlation_dls_%s.ps', $dls_trial_id);
-        my $count_rate_ps_file_name   = $ps_dir->file(sprintf 'count_rate_dls_%s.ps', $dls_trial_id);
-        my $correlation_img_file_name = $img_dir->file(sprintf 'correlation_dls_%s.png', $dls_trial_id);
-        my $count_rate_img_file_name  = $img_dir->file(sprintf 'count_rate_dls_%s.png', $dls_trial_id);
-        my $param_file_name           = $param_dir->file(sprintf 'count_rate_param_%s.txt', $dls_trial_id);
-        my $fit_file_name             = $plt_dir->file(sprintf 'fit_dls_%s.plt', $dls_trial_id);
-        my $log_file_name             = $log_dir->file(sprintf 'fit_log_%s.txt', $dls_trial_id);
+        my $correlation_dat_file_name = dat_dir->file(sprintf 'correlation_dls_%s.dat', $dls_trial_id);
+        my $count_rate_dat_file_name  = dat_dir->file(sprintf 'count_rate_dls_%s.dat', $dls_trial_id);
+#         my $correlation_plt_file_name = plt_dir->file(sprintf 'correlation_dls_%s.plt', $dls_trial_id);
+#         my $count_rate_plt_file_name  = plt_dir->file(sprintf 'count_rate_dls_%s.plt', $dls_trial_id);
+        my $correlation_ps_file_name  = ps_dir->file(sprintf 'correlation_dls_%s.ps', $dls_trial_id);
+        my $count_rate_ps_file_name   = ps_dir->file(sprintf 'count_rate_dls_%s.ps', $dls_trial_id);
+        my $correlation_img_file_name = img_dir->file(sprintf 'correlation_dls_%s.png', $dls_trial_id);
+        my $count_rate_img_file_name  = img_dir->file(sprintf 'count_rate_dls_%s.png', $dls_trial_id);
+        my $param_file_name           = param_dir->file(sprintf 'count_rate_param_%s.txt', $dls_trial_id);
+        my $fit_file_name             = plt_dir->file(sprintf 'fit_dls_%s.plt', $dls_trial_id);
+        my $log_file_name             = log_dir->file(sprintf 'fit_log_%s.txt', $dls_trial_id);
         my $correlation_dat_file      = IO::File->new($correlation_dat_file_name, 'w');
         my $count_rate_dat_file       = IO::File->new($count_rate_dat_file_name, 'w');
-        my $correlation_plt_file      = IO::File->new($correlation_plt_file_name, 'w');
-        my $count_rate_plt_file       = IO::File->new($count_rate_plt_file_name, 'w');
+#         my $correlation_plt_file      = IO::File->new($correlation_plt_file_name, 'w');
+#         my $count_rate_plt_file       = IO::File->new($count_rate_plt_file_name, 'w');
         $correlation_dat_file->print (join '', @correlation);
         $count_rate_dat_file->print (join '', @count_rate);
+        $correlation_dat_file->close;
+        $count_rate_dat_file->close;
 
         my $correlation_title = '';
         my $count_rate_title = '';
@@ -103,49 +114,55 @@ for my $date (sort grep {/\d+/} $asc_dir->open->read) {
             log_file_name   => $log_file_name,
             statement       => qq{
 set xrange [0.01:1]
+tau = 0.1
+beta = 1
 fit y_0 + A * exp(-(x/tau)** beta) '$correlation_dat_file_name' via y_0, A, tau, beta
 },
             param_file_name => $param_file_name,
-        );
+        ) or next;
         my $tau = $res->{tau};
+        my $a = $res->{A};
+        my $beta = $res->{beta};
+        my $y = $res->{y_0};
         my $fit_wssr = $res->{FIT_WSSR};
         my $fit_stdfit = $res->{FIT_STDFIT};
         $dls_trial->relaxation_time($tau);
+        $dls_trial->a($a);
+        $dls_trial->beta($beta);
+        $dls_trial->y($y);
         $dls_trial->fit_wssr($fit_wssr);
         $dls_trial->fit_stdfit($fit_stdfit);
-        $correlation_plt_file->print (<<EOD);
-set term postscript
-set yrange[0:$correlation_max]
-set logscale x
-set key outside
-set output '$correlation_ps_file_name'
-plot '$correlation_dat_file_name' t '$correlation_title'
-EOD
-        $count_rate_plt_file->print (<<EOD);
-set term postscript
-set key outside
-set output '$count_rate_ps_file_name'
-plot '$count_rate_dat_file_name' w l t '$count_rate_title'
-EOD
-        $correlation_dat_file->close;
-        $count_rate_dat_file->close;
-        $correlation_plt_file->close;
-        $count_rate_plt_file->close;
-        Q10::Gnuplot->run($correlation_plt_file_name);
-        Q10::Gnuplot->convert($correlation_ps_file_name, $correlation_img_file_name);
-        Q10::Gnuplot->run($count_rate_plt_file_name);
-        Q10::Gnuplot->convert($count_rate_ps_file_name, $count_rate_img_file_name);
-        $graph_html .= sprintf qq{
-<h3>%s : </h3>
-<img src="%s" width=600 height=400 />
-<img src="%s" width=600 height=400 />
-\n}, $dls_trial_id, $correlation_img_file_name, $count_rate_img_file_name;
+#         $correlation_plt_file->print (<<EOD);
+# set term postscript
+# set yrange[0:$correlation_max]
+# set logscale x
+# set key outside
+# set output '$correlation_ps_file_name'
+# plot '$correlation_dat_file_name' t '$correlation_title'
+# EOD
+#         $count_rate_plt_file->print (<<EOD);
+# set term postscript
+# set key outside
+# set output '$count_rate_ps_file_name'
+# plot '$count_rate_dat_file_name' w l t '$count_rate_title'
+# EOD
+#         $correlation_plt_file->close;
+#         $count_rate_plt_file->close;
+#         Q10::Gnuplot->run($correlation_plt_file_name);
+#         Q10::Gnuplot->convert($correlation_ps_file_name, $correlation_img_file_name);
+#         Q10::Gnuplot->run($count_rate_plt_file_name);
+#         Q10::Gnuplot->convert($count_rate_ps_file_name, $count_rate_img_file_name);
+#         $graph_html .= sprintf qq{
+# <h3>%s : </h3>
+# <img src="%s" width=600 height=400 />
+# <img src="%s" width=600 height=400 />
+# \n}, $dls_trial_id, $correlation_img_file_name, $count_rate_img_file_name;
     }
 }
 
-my $graph_html_file = IO::File->new(dir($Bin, 'html')->file('dls.html'), 'w');
-$graph_html_file->print($graph_html);
-$graph_html_file->close;
+# my $graph_html_file = IO::File->new(dir($Bin, 'html')->file('dls.html'), 'w');
+# $graph_html_file->print($graph_html);
+# $graph_html_file->close;
 
 
 
