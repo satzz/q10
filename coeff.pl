d4825e2e67a33bebe50a2b41bfa0c11978977a49
satzz21@gmail.com <satodaakihiko@apple-of-satzz.local>
2009-02-18 12:42:23 +0900

    modify coeff.pl

--- coeff.pl
+++ coeff.pl
@@ -7,6 +7,7 @@ use Data::Dumper;
 use Path::Class;
 use Q10::MoCo;
 use Q10::Config;
+use Q10::Divider;
 use Q10::Gnuplot;
 
 my $dat_file_name    = dat_dir->file('coeff.dat');
@@ -18,43 +19,61 @@ my $dat_file = IO::File->new($dat_file_name, 'w');
 my $dat_ln_file = IO::File->new($dat_ln_file_name, 'w');
 my $plt_file = IO::File->new($plt_file_name, 'w');
 
-my @record = moco('TempertureP8Ratio')->search(order => 'p8_ratio asc, temperture asc');
-my ($old_p8_ratio, $p8_ratio);
-my $index = -1;
-my @plot;
-my $plot;
-my $out;
-for my $record (@record) {
-    $p8_ratio = $record->p8_ratio;
-    warn $p8_ratio;
-    if (!defined $old_p8_ratio or $old_p8_ratio != $p8_ratio) {
-        $out .= "\n\n" if defined $old_p8_ratio;
-        $out .= sprintf "# %s%% P8\n", $p8_ratio;
-        $index++;
-        push @plot, sprintf qq{'%s' ind %s  w l t '%s%% P8'}, $dat_file_name, $index, $p8_ratio;
-    }
-    my $temperture = $record->temperture;
-    my $b = $record->b;
-    $out .= sprintf "%s\t%s\n", $temperture, $b;
-    $old_p8_ratio = $p8_ratio;
+{
+    my $html_file_name = html_dir->file('temperture.html');
+    my $html_file = IO::File->new($html_file_name, 'w');
+    my $html = '';
+    my $divider1 = Q10::Divider->new;
+    $divider1->model      = 'TempertureP8Ratio';
+    $divider1->divider_model      = 'TempertureP8Ratio';
+    $divider1->divider    = 'temp_column';
+    $divider1->key        = 'p8_ratio';
+    $divider1->x          = 'temperture';
+    $divider1->y          = 'b';
+    $divider1->with_lines = 1;
+    $divider1->run;
+    $html .= $divider1->get_html;
+    $html_file->print($html);
+    $html_file->close;
 }
-$plot = join ',', @plot;
-$dat_file->print($out);
-$dat_file->close;
-$dat_file->close;
-$out = qq{
-set term postscript
-set key outside
-set xlabel 'temperture[deg C]'
-set ylabel 'b'
-set xrange [600:1600]
 
-set output '$ps_file_name'
-plot $plot
-};
-$plt_file->print($out);
-Q10::Gnuplot->run($plt_file_name);
-Q10::Gnuplot->convert($ps_file_name, $img_file_name);
+# my @record = moco('TempertureP8Ratio')->search(order => 'p8_ratio asc, temperture asc');
+# my ($old_p8_ratio, $p8_ratio);
+# my $index = -1;
+# my @plot;
+# my $plot;
+# my $out;
+# for my $record (@record) {
+#     $p8_ratio = $record->p8_ratio;
+#     warn $p8_ratio;
+#     if (!defined $old_p8_ratio or $old_p8_ratio != $p8_ratio) {
+#         $out .= "\n\n" if defined $old_p8_ratio;
+#         $out .= sprintf "# %s%% P8\n", $p8_ratio;
+#         $index++;
+#         push @plot, sprintf qq{'%s' ind %s  w l t '%s%% P8'}, $dat_file_name, $index, $p8_ratio;
+#     }
+#     my $temperture = $record->temperture;
+#     my $b = $record->b;
+#     $out .= sprintf "%s\t%s\n", $temperture, $b;
+#     $old_p8_ratio = $p8_ratio;
+# }
+# $plot = join ',', @plot;
+# $dat_file->print($out);
+# $dat_file->close;
+# $dat_file->close;
+# $out = qq{
+# set term postscript
+# set key outside
+# set xlabel 'temperture[deg C]'
+# set ylabel 'b'
+# set xrange [600:1600]
+
+# set output '$ps_file_name'
+# plot $plot
+# };
+# $plt_file->print($out);
+# Q10::Gnuplot->run($plt_file_name);
+# Q10::Gnuplot->convert($ps_file_name, $img_file_name);
 
 
 
