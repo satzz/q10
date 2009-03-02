56190dac2af0c908c188bcd7214cd6da7c12b7a5
satzz21@gmail.com <satodaakihiko@EM114-51-140-211.pool.e-mobile.ne.jp>
2009-03-03 05:58:16 +0900

    modify data

--- schema.sql
+++ schema.sql
@@ -63,79 +63,96 @@ INSERT INTO microscope_trial (microscope_trial_id, cell_id, date) VALUES (27, 14
 INSERT INTO microscope_trial (microscope_trial_id, cell_id, date) VALUES (28, 15, '2009-02-22');
 INSERT INTO microscope_trial (microscope_trial_id, cell_id, date) VALUES (29, 16, '2009-02-25');
 INSERT INTO microscope_trial (microscope_trial_id, cell_id, date) VALUES (30, 17, '2009-02-25');
+INSERT INTO microscope_trial (microscope_trial_id, cell_id, date) VALUES (31, 14, '2009-02-28');
+INSERT INTO microscope_trial (microscope_trial_id, cell_id, date) VALUES (32, 15, '2009-02-28');
+INSERT INTO microscope_trial (microscope_trial_id, cell_id, date) VALUES (33, 16, '2009-03-01');
+INSERT INTO microscope_trial (microscope_trial_id, cell_id, date) VALUES (34, 17, '2009-03-01');
 
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (1,  'i_to_n', 1519);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (1,  'n_to_cry', 747);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (1,  'n_to_b4', 1030);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (1,  'i_to_b4', 1030);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (2,  'i_to_n', 1450);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (2,  'n_to_cry', 765);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (2,  'n_to_b4', 1108);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (2,  'i_to_b4', 1108);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (3,  'i_to_n', 1606);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (3,  'n_to_cry', 800);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (4,  'i_to_n', 1588);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (4,  'n_to_cry', 757);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (4,  'n_to_b4', 575);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (4,  'i_to_b4', 575);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (5,  'i_to_n', 1507);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (5,  'n_to_cry', 764);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (5,  'n_to_b4', 892);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (5,  'i_to_b4', 892);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (6,  'i_to_n', 1563);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (6,  'n_to_cry', 745);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (6,  'n_to_b4', 725);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (6,  'i_to_b4', 725);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (7,  'i_to_n', 1529);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (7,  'n_to_cry', 767);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (7,  'n_to_b4', 968);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (7,  'i_to_b4', 968);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (8,  'i_to_n', 1590);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (8,  'n_to_cry', 782);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (8,  'n_to_b4', 662);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (8,  'i_to_b4', 662);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (9,  'i_to_n', 1545);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (9,  'n_to_cry', 804);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (9,  'n_to_b4', 950);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (9,  'i_to_b4', 950);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (10,  'i_to_n', 1543);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (10,  'n_to_cry', 761);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (10,  'n_to_b4', 710);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (10,  'i_to_b4', 710);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (11,  'i_to_n', 1504);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (11,  'n_to_cry', 769);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (11,  'n_to_b4', 879);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (11,  'i_to_b4', 879);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (12,  'i_to_n', 1719);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (12,  'n_to_b4', 1175);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (12,  'i_to_b4', 1175);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (13,  'i_to_n', 1538);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (13,  'n_to_b4', 958);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (13,  'i_to_b4', 958);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (14,  'i_to_n', 1334);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (14,  'n_to_cry', 711);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (14,  'n_to_b4', 1213);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (14,  'i_to_b4', 1213);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (15,  'i_to_n', 1546);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (15,  'n_to_cry', 780);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (15,  'n_to_b4', 563);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (15,  'i_to_b4', 563);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (16,  'i_to_n', 1355);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (17,  'i_to_n', 1395);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (17,  'n_to_b4', 1273);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (17,  'i_to_b4', 1273);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (18,  'i_to_n', 1362);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (19,  'n_to_b4', 1271);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (19,  'i_to_b4', 1271);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (20,  'n_to_cry', 763);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (21,  'i_to_n', 1396);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (21,  'n_to_b4', 1163);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (21,  'i_to_b4', 1163);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (22,  'i_to_n', 1410);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (22,  'n_to_cry', 780);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (22,  'n_to_b4', 1190);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (22,  'i_to_b4', 1190);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (23,  'i_to_n', 1410);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (23,  'n_to_cry', 810);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (23,  'n_to_b4', 1210);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (23,  'i_to_b4', 1210);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (24,  'i_to_n', 1415);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (24,  'n_to_cry', 790);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (24,  'n_to_b4', 1197);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (24,  'i_to_b4', 1197);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (25,  'i_to_n', 1715);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (25,  'n_to_cry', 915);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (26,  'i_to_n', 1605);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (26,  'n_to_cry', 865);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (26,  'n_to_b4', 1000);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (26,  'i_to_b4', 1000);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (27,  'i_to_n', 1400);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (27,  'n_to_b4', 1152);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (27,  'i_to_b4', 1152);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (28,  'i_to_n', 1520);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (28,  'n_to_b4', 821);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (28,  'i_to_b4', 921);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (29,  'i_to_n', 1645);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (29,  'n_to_cry', 793);
-INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (29,  'n_to_b4', 620);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (29,  'i_to_b4', 620);
 INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (30,  'i_to_n', 1615);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (30,  'n_to_cry', 858);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (31,  'i_to_n', 1413);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (31,  'i_to_b4', 1154);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (32,  'i_to_n', 1536);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (32,  'n_to_cry', 807);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (32,  'i_to_b4', 935);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (33,  'i_to_n', 1643);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (33,  'n_to_cry', 889);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (33,  'i_to_b4', 674);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (34,  'i_to_n', 1660);
+INSERT INTO microscope_trial_phase_transition (microscope_trial_id, phase_transition_type, temperture) VALUES (34,  'n_to_cry', 897);
+
+
 
 
 
@@ -221,4 +238,6 @@ select date,  temperture, p8_ratio, rotation_angle, count_rate_max, 1/relaxation
 
 
 
+select temperture, count(*) from dls_trial group by temperture order by temperture;
+select cell_id, count(*) from dls_trial group by cell_id order by cell_id;
 
